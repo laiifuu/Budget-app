d413cf9f75a9718bae9a7bf5178026dfcff5164b
Hayet Fellah <hayet.fellah145@gmail.com>
2023-02-13 11:11:04 +0100

    Add tables, models, views and controllers

---
+++ spec/views/entities/edit.html.erb_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "entities/edit", type: :view do
+  let(:entity) {
+    Entity.create!(
+      name: "MyString",
+      author_id: "",
+      amount: 1
+    )
+  }
+
+  before(:each) do
+    assign(:entity, entity)
+  end
+
+  it "renders the edit entity form" do
+    render
+
+    assert_select "form[action=?][method=?]", entity_path(entity), "post" do
+
+      assert_select "input[name=?]", "entity[name]"
+
+      assert_select "input[name=?]", "entity[author_id]"
+
+      assert_select "input[name=?]", "entity[amount]"
+    end
+  end
+end
---
+++ spec/views/expenses/show.html.erb_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "expenses/show", type: :view do
+  before(:each) do
+    assign(:expense, Expense.create!(
+      group_id: "",
+      entity_id: ""
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(//)
+    expect(rendered).to match(//)
+  end
+end
