4884e658ee5f53e3a41f616c05f606c6a3970a20
Hayet Fellah <hayet.fellah145@gmail.com>
2023-02-14 15:35:53 +0100

    Add JS related to the icon picker popup

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,32 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// I write my js here okay ? 
+
+let openPopupBtn = document.querySelector('.select-icon');
+openPopupBtn.addEventListener('click', (e) => {
+  let popupSection = document.querySelector('.icon-picker')
+  if (popupSection.classList.contains('hidden')){
+    popupSection.classList.remove('hidden');
+  }
+});
+
+let closePopupBtn = document.querySelector('.close-icon-picker-btn');
+closePopupBtn.addEventListener('click', (e) => {
+  let popupSection = document.querySelector('.icon-picker')
+  if (!popupSection.classList.contains('hidden')){
+    popupSection.classList.add('hidden');
+  }
+});
+
+let icons = Array.from(document.querySelectorAll('.icon'))
+icons.forEach((icon) => {
+  icon.addEventListener('click', (e) => {
+    let iconInput = document.querySelector('.icon-input');
+    let iconElement = icon.querySelector('i');
+    iconInput.value = Array.from(iconElement.classList).join(" ");
+    let popupSection = document.querySelector('.icon-picker')
+    popupSection.classList.add('hidden');
+  });
+});
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,6 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
