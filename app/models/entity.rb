6543ec0eb03c6e8947b8b3ea64cbda881d8fd896
Hayet Fellah <hayet.fellah145@gmail.com>
2023-02-15 00:12:01 +0100

    Add validations

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -2,4 +2,8 @@ class Entity < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :expenses
   has_many :groups, through: :expenses
+  
+  validates :name, presence: true
+  validates :amount, presence: true
+  validates :author_id, presence: true
 end
