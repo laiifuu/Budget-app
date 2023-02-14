f205806fa6c804f5fc277b88a887ed9562555d80
Hayet Fellah <hayet.fellah145@gmail.com>
2023-02-14 15:32:50 +0100

    Add associations

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,2 +1,5 @@
 class Entity < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :expenses
+  has_many :groups, through: :expenses
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,17 @@
 class Group < ApplicationRecord
+  belongs_to :user
+  has_many :expenses
+  has_many :entities, through: :expenses
+
+  validates :name, presence: true
+  validates :icon, presence: true
+
+  def total_amount 
+    entities = self.entities
+    total = 0;
+    entities.each do |entity| 
+      total+= entity.amount
+    end
+    total
+  end 
 end
