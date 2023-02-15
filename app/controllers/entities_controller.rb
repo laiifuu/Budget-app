6b328618a41a7bea4a5ac4f53cdbbe568b35041b
Hayet Fellah <hayet.fellah145@gmail.com>
2023-02-15 11:26:28 +0100

    Add login condition before accessing the app

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,5 +1,6 @@
 class EntitiesController < ApplicationController
   before_action :set_entity, only: %i[show edit update destroy]
+  before_action :authenticate_user!
 
   # GET /entities or /entities.json
   def index
