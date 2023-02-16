0f6c8c4051f2dd304b672c0340e6def622ce9fec
Hayet Fellah <hayet.fellah145@gmail.com>
2023-02-16 15:38:01 +0100

    Remove useless routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :entities, only: [:index, :show, :new, :create] 
+  resources :entities, only: [:new, :create] 
   resources :groups, only: [:index, :show, :new, :create] 
-  resources :expenses
   resources :users
 
   root "application#index"
